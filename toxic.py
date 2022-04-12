ba534a7a01582a25c400ab7d282ca285b671d0f0
Toxicvai <102881416+Toxicvai@users.noreply.github.com>
2022-04-13 00:30:56 +0800

    Add files via upload

---
+++ toxic.py
@@ -0,0 +1,22 @@
+usr = "MBR69"
+pwd = "mbr69"
+while True:
+  user = input("Enter username ")
+  if user == usr:
+  	print(user)
+  	Password = input("Enter Password:")
+  if Password == pwd:
+    print("Welcome admin")
+    break
+  else:
+  	print("try again")
+
+
+
+i = 1
+while i <= 1000000000000000000000:
+  print ("I love u")
+  i+= 1
+ 
+print ("\n\n love u @3:\n\n")
+
